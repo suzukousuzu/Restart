41ca31fcd897dc3051157c92dcdf633e6a2c09b3
Kota Suzuki <addressuzuki@gmail.com>
2024-02-06 08:26:25 +0900

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by 鈴木航太 on 2024/02/06.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
